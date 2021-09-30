bef8ea9e3cc0726e2c2a8efd493463c7bf3a5e52
Dmitriy Shadurskiy <dnbdimka@gmail.com>
2021-09-30 09:00:44 +0300

    update

--- src/Components/App.js
+++ src/Components/App.js
@@ -11,7 +11,8 @@ class App extends Component {
   };
 
   countTotalFeedback = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
+    const {good, neutral, bad} = this.state
+    return good + neutral + bad;
   };
 
   countPositiveFeedbackPercentage = () => {
